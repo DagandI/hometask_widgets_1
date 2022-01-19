aec160ce4c79c37e81afdb65ce85db2c1c3c34c5
Murad Magomedov <magomedov93murad@gmail.com>
2022-01-20 00:03:45 +0300

    lesson 4 before Homework

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_fight_club/resources/fight_club_colors.dart';
+import 'package:flutter_fight_club/resources/fight_club_images.dart';
+import 'package:flutter_fight_club/widgets/action_button.dart';
+import 'package:flutter_fight_club/pages/fight_page.dart';
+import 'package:flutter_fight_club/pages/main_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.pressStart2pTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: MainPage(),
+    );
+  }
+}
